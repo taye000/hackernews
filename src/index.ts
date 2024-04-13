527fbfd2cc9810e1a6da79a0326c3b7d8cd048b3
taye000 <taylorgitari@gmail.com>
2024-04-13 15:30:48 +0300

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,19 @@
-import express, { Request, Response } from "express";
-import {
-  getGraphQLParameters,
-  processRequest,
-  shouldRenderGraphiQL,
-  renderGraphiQL,
-} from "graphql-helix";
+import { createYoga } from "graphql-yoga";
 import "graphql-import-node";
 import { schema } from "./schema/schema";
+import { createServer } from "http";
 
 async function main() {
-  const app = express();
+  const yoga = createYoga({
+    schema,
+  });
+
+  const server = createServer(yoga);
+
   const PORT = process.env.PORT || 3000;
 
-  // Middleware to parse JSON requests
-  app.use(express.json());
-
-  // Test route
-  app.get("/", (req: Request, res: Response) => {
-    res.send({ test: true });
-  });
-
-  // GraphQL endpoint
-  app.all("/graphql", async (req: Request, res: Response) => {
-    const request = {
-      headers: req.headers,
-      method: req.method,
-      query: req.query,
-      body: req.body,
-    };
-
-    if (shouldRenderGraphiQL(request)) {
-      res.header("Content-Type", "text/html");
-      res.send(
-        renderGraphiQL({
-          endpoint: "/graphql",
-        })
-      );
-      return;
-    }
-
-    const { operationName, query, variables } = getGraphQLParameters(request);
-
-    const result = await processRequest({
-      request,
-      schema,
-      operationName,
-      query,
-      variables,
-    });
-
-    res.setHeader("Content-Type", "application/json");
-    res.send(result);
-  });
-
   // Start the server
-  app.listen(PORT, () => {
+  server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
   });
 }
